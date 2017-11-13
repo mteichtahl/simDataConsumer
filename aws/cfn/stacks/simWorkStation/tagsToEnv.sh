db07bfda98f0d78bd4cb0185f2875c44937f65f3
Marc Teichtahl <marc@teichtahl.com>
2017-11-13 12:51:53 +1100

    updated

---
+++ aws/cfn/stacks/simWorkStation/tagsToEnv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+get_instance_tags () {
+    instance_id=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/instance-id)
+    echo $(aws ec2 describe-tags --region ap-southeast-2 --filters "Name=resource-id,Values=$instance_id")
+}
+
+get_ami_tags () {
+    ami_id=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/ami-id)
+    echo $(aws ec2 describe-tags --region ap-southeast-2 --filters "Name=resource-id,Values=$ami_id")
+}
+
+tags_to_env () {
+    tags=$1
+
+    for key in $(echo $tags | jq -r ".[][].Key"); do
+        value=$(echo $tags | jq -r ".[][] | select(.Key==\"$key\") | .Value")
+        key=$(echo $key | tr '-' '_' | tr ':' '_' | tr '[:lower:]' '[:upper:]')
+        export $key="$value"
+        echo export $key="$value"
+    done
+}
+
+ami_tags=$(get_ami_tags)
+instance_tags=$(get_instance_tags)
+
+tags_to_env "$ami_tags"
+tags_to_env "$instance_tags"
