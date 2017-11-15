84d62152c3b382a1c8341dd976592c306056b3b4
Marc Teichtahl <marc@teichtahl.com>
2017-11-15 21:41:44 +1100

    updates for easy of deployment

--- aws/cfn/stacks/upload.sh
+++ aws/cfn/stacks/upload.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 aws s3 cp APIGatewayAndLambdaStack.json s3://reinvent-abd322-workshop/APIGatewayAndLambdaStack.json --acl public-read
-echo "https://s3-ap-southeast-2.amazonaws.com/reinvent-abd322-workshop/APIGatewayAndLambdaStack.json"
+
+aws s3 cp simulatorWorkstationStack.json s3://reinvent-abd322-workshop/simulatorWorkstationStack.json --acl public-read
+aws s3 cp lambdaFunctionAndDDBStack.json s3://reinvent-abd322-workshop/lambdaFunctionAndDDBStack.json --acl public-read
+
+
+echo "https://s3-ap-southeast-2.amazonaws.com/reinvent-abd322-workshop/APIGatewayAndLambdaStack.json\n"
+echo "https://s3-ap-southeast-2.amazonaws.com/reinvent-abd322-workshop/lambdaFunctionAndDDBStack.json\n"
+echo "https://s3-ap-southeast-2.amazonaws.com/reinvent-abd322-workshop/simulatorWorkstationStack.json\n"
